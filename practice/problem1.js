41ca86f8c9cbcf87995b2c2c32f2c1ab6e03e862
mithlesh-yogi <mithleshyogi8955@gmail.com>
2026-08-17 20:41:25 +0530

    practicing js

---
+++ practice/problem1.js
@@ -0,0 +1,10 @@
+const arr = [3,5,6,7,2,15];
+let maxvalue =arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i]>maxvalue) {
+        maxvalue= arr[i];
+    }
+}
+let maximum_value = maxvalue;
+console.log(maximum_value);
